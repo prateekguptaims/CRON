fc5cc7f16b7d36f6704036839d4f451e5dd2ebd5
Prateek <56671784+prateekguptaims@users.noreply.github.com>
2024-10-27 15:33:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+app.get('/',(req,res) => {
+    res.send('hello');
+})
 // Debugging: Log the username and password
 console.log('Username:', process.env.MY_USERNAME);
 console.log('Password:', process.env.MY_PASSWORD);
